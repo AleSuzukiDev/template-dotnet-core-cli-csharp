46bbdbb8d331e5928c599fd920b9ebdbaa2c852a
Alessandra Suzuki <alesuzukidev@gmail.com>
2022-04-26 00:10:31 +0000

    commit 2

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace dotnetcore
         static void Main(string[] args)
         {
             // Declaração que exibe alguma informação no console
-            Console.WriteLine("Hello World!"); 
+            //Console.WriteLine("Hello World!"); 
             
             //Guardar uma informação e utilizar depois
             //variavel
@@ -15,18 +15,68 @@ namespace dotnetcore
             //1ª coisa que precisamos: tipo da informação
             //2ª coisa que precisamos: nome da variável. Esse nome não pode ter espaços, caracteres especiais ou começar com número
 
-            //Meu nome é Alessandra Suzuki
-            string nome;
             
-            //Aqui atribuo o conteúdo a string
-            nome = "Alessandra Suzuki";
+            #region DeclarandoNomes
+            /*
+                //Meu nome é Alessandra Suzuki
+                string nome;
+                
+                //Aqui atribuo o conteúdo a string
+                nome = "Alessandra Suzuki";
 
-            //pode ser assim também. Desta forma eu declaro e ja atribuo
-            string nome2 = "Alessandra Suzuki Mariano";
+                //pode ser assim também. Desta forma eu declaro e ja atribuo
+                string nome2 = "Alessandra Suzuki Mariano";
 
-            Console.WriteLine(nome);
+                Console.WriteLine(nome);
+                Console.WriteLine(nome2);
+            */
+            #endregion
             
-            Console.WriteLine(nome2);
+
+            
+            
+            #region DeclaracaoPorAssociação
+            /*
+            //Tipos de variáveis podem ser opcionais se o C# conseguir determina-los por associação.
+            var nome = "Ale";
+            var sobrenome = "Suzuki";
+            */
+            #endregion
+            
+
+           
+            #region ConcatenacaoDeVariaveis
+             /*
+            //Tipos de variáveis podem ser opcionais se o C# conseguir determina-los por associação.
+            var nome = "Ale";
+            var sobrenome = "Suzuki";
+
+            //Concatenação (unir informações de forma lógica)
+            Console.WriteLine(nome + " " + sobrenome);
+
+            var nomeCompleto = nome + " " + sobrenome; 
+            Console.WriteLine(nomeCompleto);
+            */
+            #endregion
+
+
+            #region InterpolacaoDeVariaveis
+            /*
+            var nome = "Alessandra";
+            var sobrenome = "Suzuki";
+            var nomeCompleto = $"{nome} {sobrenome}";
+            Console.WriteLine(nomeCompleto);
+            */
+            #endregion
+
+            #region PegarInformacao
+            
+            Console.WriteLine("Digite o seu nome: ");
+            var nome = Console.ReadLine();
+            Console.WriteLine($"Olá, {nome}!");
+            
+            #endregion
+
         }
     }
 }
